835fd1031d3f53de56b5ed93f47ea62c3eeeee0c
jeetpaul12 <123904141+jeetpaul12@users.noreply.github.com>
2025-02-08 09:09:37 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const global = {
+    owner: [1382801385], // GANTI PAKE ID TELE MU YA GANTENG 
+    botToken: "7156435713:AAFtpqlVzUTvvkxvajcsiZsMXEiOfGX4FxU", // ISI BOT TOKEN LU YAK
+}
+
+module.exports = global;
+// Bocoran By Limmvz
+// https://whatsapp.com/channel/0029VawTuv09Gv7VHSUvYi37
